ce801f6ce95a117e9640ae0ef70352ebabc91ea2
Nethum <nethumnenula2004@gmail.com>
2026-07-21 16:09:08 +0530

    Added more appropriate database connection validation function. (server.js)

--- backend/server.js
+++ backend/server.js
@@ -5,30 +5,43 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
-
 // middleware
+app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
-    console.log("Middleware executed!");
-    console.log(`Method: ${req.method}`);
-    console.log(`Path: ${req.path}`);
-    next();
-})
-
-
-
+  console.log("Middleware executed!");
+  console.log(`Method: ${req.method}`);
+  console.log(`Path: ${req.path}`);
+  next();
+});
 
 // DB Connection
-mongoose
-.connect(process.env.MONGO_LOCAL)
-.then(() => {
+/*mongoose
+  .connect(process.env.MONGO_LOCAL)
+  .then(() => {
     // Listen on the PORT
     app.listen(process.env.PORT, () => {
-        console.log(`Connected to database!`);
-        console.log(`Listening on PORT ${process.env.PORT}`)
-    })
-})
-.catch((error) => {
+      console.log(`Connected to database!`);
+      console.log(`Listening on PORT ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
     console.log(`An error occured, ${error}`);
-})
+  });*/
+
+// DB connection (Most suitable)
+async function startServer() {
+  try {
+    await mongoose.connect(process.env.MONGO_LOCAL);
+    console.log("Connected to Database!");
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Listening on PORT ${process.env.PORT}.`);
+    });
+  } catch (error) {
+    console.error(`An error occurred, ${error}`);
+    process.exit(1);
+  }
+}
+
+startServer();
