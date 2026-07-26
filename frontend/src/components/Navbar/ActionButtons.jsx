19bd797a793a26853eb0ab755fed6b8c984368d3
Nethum <nethumnenula2004@gmail.com>
2026-07-26 22:21:38 +0530

    Added cursor-pointer property to Nav-Buttons

--- frontend/src/components/Navbar/ActionButtons.jsx
+++ frontend/src/components/Navbar/ActionButtons.jsx
@@ -7,14 +7,14 @@ const ActionButtons = () => {
             <button
                 className="px-5 py-2 rounded-lg border border-emerald-500
                 text-emerald-500 hover:bg-emerald-500 hover:text-white
-                transition"
+                transition-all cursor-pointer"
             >
                 Sign In
             </button>
 
             <button
                 className="px-5 py-2 rounded-lg bg-emerald-500
-                text-white hover:bg-emerald-600 transition"
+                text-white hover:bg-emerald-600 transition-all cursor-pointer"
             >
                 Get Started
             </button>
