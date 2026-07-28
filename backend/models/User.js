19c2db63ac400119a9202c71caad37b305c0bd4e
Nethum <nethumnenula2004@gmail.com>
2026-07-28 12:55:43 +0530

    Added(Backend) : More details to user schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,27 +1,37 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, "Name is required."],
-        trim: true
+  name: {
+    type: String,
+    required: [true, "Name is required."],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required."],
+    trim: true,
+  },
+  telephone: {
+    type: Number,
+    required: [true, "Telephone Number is required."],
+  },
+  vehicleDetails: {
+    prefix: {
+      type: String,
+      required: true,
     },
-    email: {
-        type: String,
-        required: [true, "Email is required."],
-        trim: true
+    suffix: {
+      type: Number,
+      required: true,
     },
-    telephone: {
-        type: Number,
-        required: [true, "Telephone Number is required."],
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-    updatedAt: {
-        type: Date,
-        default: Date.now
-    }
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 module.exports = mongoose.model("User", userSchema);
