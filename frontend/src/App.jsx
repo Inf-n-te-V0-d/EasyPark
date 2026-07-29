1fffce38cdc6e659297a841a1a34fcce82bfc121
Adisha Hirushan <adishahirushan@gmail.com>
2026-07-29 15:11:04 +0530

    create login page navigations and create register(sign up form) with navigation correctly

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,25 @@ import "./App.css";
 // Components
 import Home from "./pages/Home";
 import QrScanner from "./pages/Qr_scanner";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
     const [page, setPage] = useState("home");
 
-    return page === "qr" ? <QrScanner onNavigate={setPage} /> : <Home onNavigate={setPage} />;
+    if (page === "qr") {
+        return <QrScanner onNavigate={setPage} />;
+    }
+
+    if (page === "login") {
+        return <Login onNavigate={setPage} />;
+    }
+
+    if (page === "register") {
+        return <Register onNavigate={setPage} />;
+    }
+
+    return <Home onNavigate={setPage} />;
 }
 
 export default App;
